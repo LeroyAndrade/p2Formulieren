ad76d71146cad3fe30d5567df8a58738a93663be
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2019-12-05 13:45:30 +0100

    UPDATE .php bestand

--- pagina.php
+++ pagina.php
@@ -1,5 +1,7 @@
 <?php
  session_start();
 
-echo $_GET["w110"]. $_GET["naam1"];
+echo $_GET["naam1"]. $_GET["selecteerKlas"]." ". $_GET["positief"]." ".$_GET["positief2"]." ".$_GET["positief3"]." "
+.$_GET["sterkeKant"]." ".$_GET["zelfReflectie"]. $_GET["sterkeKant"]." ".$_GET["motivatie"]." ".$_GET["textGebied"];
+//. $_GET["sterkeKant"]." ".$_GET["motivatie"]." ".$_GET["textGebied"]
 ?>
