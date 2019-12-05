07df02f4a788b7b27c093a492156d2278b0a0b5d
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2019-12-05 05:59:02 +0100

    Opdracht met bonus
    Tweede push:
    +alle opdrachten
    
    Todo:  aanroepen alle PHP code om ingevoerde userinput te laten zien.

--- index.php
+++ index.php
@@ -27,61 +27,140 @@ session_start();
   <p>Om een eerste stat lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
 <form action="./pagina.php" method="get">
 
-<label for="naam1">Naam</label>
-   <input type="text" name="naam1" id="naam1" placeholder="Bv Jan Jansen">
+<label for="naam1">Naam &#42;</label>
+   <input type="text" id="naam1" name="naam1" placeholder="Vul hier je naam in" autofocus required>
+<br>
+<label>In welke klas zit jij&#63; &#42; </label>
+  <select name="selecteerKlas">
+       <option value="">selecteer jouw klas</option>
+       <option value="MD1A">MD1A</option>
+       <option value="MD1B">MD1B</option>
+  </select>
 
-<label for="">In welke klas zit jij ? * </label>
-     <option value="selectClass" selected>selecteer jouw klas</option>
-     <option value="MD1A">MD1A</option>
-     <option value="MD1B">MD1B</option>
-</select>
-<br/>  <br><br>
-  <section class="border1">
-    <h1 class="">Welbevinding</h1>
+  <br>
+
+<section class="mooi">
+  <fieldset>
+    <legend>Welbevinding</legend>
     <br>
-    <span class="lheight">Hoe positief ben je op dit moment over je opleiding</span><br/><br/>
+    <p>Hoe positief ben je op dit moment over je opleiding</p>
 
-      <input type="radio" name="secChec1" id="w100" value="1">
-        <label for="w100">1</label>
+    <label for="w1001"><input type="radio" name="positief" id="w1001" value="1">1</label>
+    <label for="w1002"><input type="radio" name="positief" id="w1002" value="2">2</label>
+    <label for="w1003"><input type="radio" name="positief" id="w1003" value="3">3</label>
+    <label for="w1004"><input type="radio" name="positief" id="w1004" value="4">4</label>
+    <label for="w1005"><input type="radio" name="positief" id="w1005" value="5">5</label>
+    <label for="w1006"><input type="radio" name="positief" id="w1006" value="6">6</label>
+    <label for="w1007"><input type="radio" name="positief" id="w1007" value="7">7</label>
+    <label for="w1008"><input type="radio" name="positief" id="w1008" value="8">8</label>
+    <label for="w1009"><input type="radio" name="positief" id="w1009" value="9">9</label>
+    <label for="w1000"><input type="radio" name="positief" id="w1000" value="10">10</label>
 
-      <input type="radio" name="secChec1" id="w110" value="2">
-        <label for="w110">2</label>
-<!--
-      <input type="radio" name="secChec1" id="w111" value="3">
-        <label for="w111">3</label>
+    <p>Hoe positief ben je op dit moment over je opleiding</p>
+
+    <label for="w10011"><input type="radio" name="positief2" id="w10011" value="1">1</label>
+    <label for="w10022"><input type="radio" name="positief2" id="w10022" value="2">2</label>
+    <label for="w10033"><input type="radio" name="positief2" id="w10033" value="3">3</label>
+    <label for="w10044"><input type="radio" name="positief2" id="w10044" value="4">4</label>
+    <label for="w10055"><input type="radio" name="positief2" id="w10055" value="5">5</label>
+    <label for="w10066"><input type="radio" name="positief2" id="w10066" value="6">6</label>
+    <label for="w10077"><input type="radio" name="positief2" id="w10077" value="7">7</label>
+    <label for="w10088"><input type="radio" name="positief2" id="w10088" value="8">8</label>
+    <label for="w10099"><input type="radio" name="positief2" id="w10099" value="9">9</label>
+    <label for="w10000"><input type="radio" name="positief2" id="w10000" value="10" checked>10</label>
+
+    <p>Ga je door met deze opleiding</p>
+
+    <label for="w100111"><input type="radio" name="positief3" id="w100111" value="1">1</label>
+    <label for="w100222"><input type="radio" name="positief3" id="w100222" value="2">2</label>
+    <label for="w100333"><input type="radio" name="positief3" id="w100333" value="3">3</label>
+    <label for="w100444"><input type="radio" name="positief3" id="w100444" value="4">4</label>
+    <label for="w100555"><input type="radio" name="positief3" id="w100555" value="5">5</label>
+    <label for="w100666"><input type="radio" name="positief3" id="w100666" value="6">6</label>
+    <label for="w100777"><input type="radio" name="positief3" id="w100777" value="7">7</label>
+    <label for="w100888"><input type="radio" name="positief3" id="w100888" value="8">8</label>
+    <label for="w100999"><input type="radio" name="positief3" id="w100999" value="9">9</label>
+    <label for="w100000"><input type="radio" name="positief3" id="w100000" value="10" checked>10</label>
+</fieldset>
+</section>
+<br>
 
 
-      <input type="radio" name="secChec1" id="w112" value="4">
-        <label for="w112">4</label>
+<section class="sterk">
+  <fieldset>
+    <legend>Sterke kanten</legend>
+    <p>Wat zijn je sterke kanten in programmeren?</p>
 
+    <label for="sterk1"><input type="checkbox" name="sterkeKant" id="sterk1">Coderen in HTML&#47;CSS</label>
+    <label for="sterk2"><input type="checkbox" name="sterkeKant" id="sterk2">Programmeren in Java</label>
+    <label for="sterk3"><input type="checkbox" name="sterkeKant" id="sterk3">Scripting&#58; Programmeren in Javascript</label>
+    <label for="sterk4"><input type="checkbox" name="sterkeKant" id="sterk4" checked>Planning</label>
+    <label for="sterk5"><input type="checkbox" name="sterkeKant" id="sterk5">Samenwerken</label>
+  </fieldset>
+</section>
 
-      <input type="radio" name="secChec1" id="w113" value="5">
-        <label for="w113">5</label>
+<section class="zelfReflecteren">
+  <fieldset>
+    <legend>Nog te ontwikkelen&#63;</legend>
+    <p>Aan welke skills moet je nog wat extra aandacht besteden&#63;</p>
 
+    <label for="zelfReflectie1"><input type="checkbox" name="zelfReflectie" id="zelfReflectie1">Coderen in HTML&#47;CSS</label>
+    <label for="zelfReflectie2"><input type="checkbox" name="zelfReflectie" id="zelfReflectie2">Programmeren in Java</label>
+    <label for="zelfReflectie3"><input type="checkbox" name="zelfReflectie" id="zelfReflectie3">Scripting&#58; Programmeren in Javascript</label>
+    <label for="zelfReflectie4"><input type="checkbox" name="zelfReflectie" id="zelfReflectie4">Planning</label>
+    <label for="zelfReflectie5"><input type="checkbox" name="zelfReflectie" id="zelfReflectie5" checked>Samenwerken</label>
+  </fieldset>
+</section>
 
-      <input type="radio" name="secChec1" id="w114" value="6">
-        <label for="w114">6</label>
+<section class="sterk">
+  <fieldset>
+    <legend>Tot nu toe</legend>
+    <p>Wat vind je het leukste vak tot nu toe&#63;</p>
 
+    <label for="TotNu1"><input type="checkbox" name="sterkeKant" id="TotNu1" checked>PRO1</label>
+    <label for="TotNu2"><input type="checkbox" name="sterkeKant" id="TotNu2" checked>PRO2</label>
+    <label for="TotNu3"><input type="checkbox" name="sterkeKant" id="TotNu3" checked>SLB</label>
+    <label for="TotNu4"><input type="checkbox" name="sterkeKant" id="TotNu4" checked>NED</label>
+    <label for="TotNu5"><input type="checkbox" name="sterkeKant" id="TotNu5" checked>ENG</label>
+    <label for="TotNu6"><input type="checkbox" name="sterkeKant" id="TotNu6" checked>PROJ</label>
+    <label for="TotNu7"><input type="checkbox" name="sterkeKant" id="TotNu7" checked>TPR</label>
+    <label for="TotNu8"><input type="checkbox" name="sterkeKant" id="TotNu8" checked>WVO</label>
+    <label for="TotNu9"><input type="checkbox" name="sterkeKant" id="TotNu9" checked>UXD</label>
+    <label for="TotNu10"><input type="checkbox" name="sterkeKant" id="TotNu10" checked>REK</label>
 
-      <input type="radio" name="secChec1" id="w115" value="7">
-        <label for="w115">7</label>
+    <p>Met hoeveel zin ga je naar school&#63;</p>
+      <section class="mooi">
+      <label for="motivatie1"><input type="radio" name="motivatie" id="motivatie1" value="1">1</label>
+      <label for="motivatie2"><input type="radio" name="motivatie" id="motivatie2" value="2">2</label>
+      <label for="motivatie3"><input type="radio" name="motivatie" id="motivatie3" value="3">3</label>
+      <label for="motivatie4"><input type="radio" name="motivatie" id="motivatie4" value="4">4</label>
+      <label for="motivatie5"><input type="radio" name="motivatie" id="motivatie5" value="5">5</label>
+      <label for="motivatie6"><input type="radio" name="motivatie" id="motivatie6" value="6">6</label>
+      <label for="motivatie7"><input type="radio" name="motivatie" id="motivatie7" value="7">7</label>
+      <label for="motivatie8"><input type="radio" name="motivatie" id="motivatie8" value="8">8</label>
+      <label for="motivatie9"><input type="radio" name="motivatie" id="motivatie9" value="9">9</label>
+      <label for="motivatie10"><input type="radio" name="motivatie" id="motivatie10" value="10" checked>10</label>
+      </section>
+        </fieldset>
+</section>
 
-
-      <input type="radio" name="secChec1" id="w116" value="8">
-        <label for="w116">8</label>
-
-
-      <input type="radio" name="secChec1" id="w117" value="9">
-        <label for="w117">9</label>
-
-
-      <input type="radio" name="secChec1" id="w118" value="10">
-        <label for="w118">10</label>
--->
-  <br><br>
+<p>Wat wil je in de toekomst nog doen&#63;</p>
+<textarea name="textGebied" placeholder="Voeg hier nog extra info bij alstublieft&#33;" rows="9" cols="42"></textarea>
+  <br>
 <input type="submit" value="Verstuur">
-
-    </section>
   </form>
+
+
+  <a href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2F30168.hosts2.ma-cloud.nl%2Fbewijzenmap%2Fperiode2%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=nl" target="_blank">
+                        <img style="border:0;width:88px;height:31px"
+                            src="http://30168.hosts2.ma-cloud.nl/bewijzenmap/periode1.1/pro1/splash/w3cHTML.png"
+                            alt="Valide CSS!" />
+                        </a>
+
+                        <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2F30168.hosts2.ma-cloud.nl%2Fbewijzenmap%2Fperiode2%2F" target="_blank">
+                            <img style="border:0;width:88px;height:31px"
+                                src="http://30168.hosts2.ma-cloud.nl/bewijzenmap/periode1.1/pro1/splash/w3CSS.png"
+                                alt="Valide CSS!" />
+                            </a>
 </body>
 </html>
